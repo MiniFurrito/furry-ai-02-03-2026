b83fa4ac62441ec191282cd4153c9d58c3669669
MiniFurrito <minifurritofurry@gmail.com>
2026-04-19 16:36:37 -0500

    fix: tailwind postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-// tailwind.config.js   ← en la raíz del proyecto
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
